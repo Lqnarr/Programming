1b1bf730ace9106055dae49a254ea49ba32b698f
Eric Gao <ericgaocan2@gmail.com>
2024-04-10 11:19:09 -0700

    2

---
+++ pythonProject/unit-mid2-activity.py
@@ -0,0 +1,36 @@
+# Calculate the sum of arrays
+# Author: Eric Gao
+# Date: Apr 10th, 2024
+
+
+def play():
+    stuff = ["Banana", "Apple", "Orange", "Pineapple", "JF Kennedy", "AP CSA", "JF Kennedy"]
+    item = "JF Kennedy"
+
+    print(f"Number of {item} is {numItem(stuff, item)}")
+
+    print()
+
+    print(f"Printing all the items until {item}:")
+    untilItem(stuff, item)
+
+
+def numItem(list, item: str):
+    item = item.strip().lower()
+    counter = 0
+    for i in list:
+        i = i.strip().lower()
+        if i == item:
+            counter += 1
+    return counter
+
+
+def untilItem(list, item:str):
+    item = item.strip().lower()
+    for i in list:
+        print(i)
+        i = i.strip().lower()
+        if i == item:
+            break
+
+play()
